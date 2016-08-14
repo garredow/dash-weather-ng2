72a1c986c816cc28098a71e5d510fad6f223de2f
Garrett <choorp@gmail.com>
2016-08-14 14:49:17 -0400

    Added temperature to hourly

--- src/client/app/hourly/hourly.component.ts
+++ src/client/app/hourly/hourly.component.ts
@@ -9,13 +9,27 @@ export class HourlyComponent implements OnInit {
 	@Input() icon: string;
 	@Input() summary: string;
 	@Input() time: number;
+	@Input() data: any;
+	@Input() previous: any;
 	iconUrl: string;
+	difference: string;
 
 	constructor() { }
 
 	ngOnInit() {
-		this.iconUrl = `./assets/icons/${this.icon}.png`;
-		this.time *= 1000;
+		this.iconUrl = `./assets/icons/${this.data.icon}.png`;
+		this.data.time *= 1000;
+		this.data.temperature = Math.round(this.data.temperature);
+
+		if (this.previous) {
+			if (this.data.temperature > this.previous.temperature) {
+				this.difference = 'warmer';
+			} else if (this.data.temperature < this.previous.temperature) {
+				this.difference = 'cooler';
+			} else {
+				this.difference = '';
+			}
+		}
 	}
 
 }
